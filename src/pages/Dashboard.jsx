39e77e36f14349a086d9776eebd33e54f52d483d
lisatlt44 <lisa.tallet1@gmail.com>
2024-04-01 15:36:11 +0200

    ajout deconnexion, suppression token local storage

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,20 @@
 import { Button } from '@nextui-org/react'
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard () {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Supprimer le token du local storage
+    localStorage.removeItem('token');
+    // Rediriger vers la page d'accueil
+    navigate('/');
+  };
+
   return (
     <>
       <h2>Dashboard</h2>
-      <Button>
+      <Button onClick={handleLogout}>
         Se déconnecter
       </Button>
     </>
